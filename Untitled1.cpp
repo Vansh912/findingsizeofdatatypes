c97e261648b701c242c7c818e3f1196866a9c209
Vansh912 <89155230+Vansh912@users.noreply.github.com>
2021-12-11 11:47:00 +0530

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(void)
+{
+	int x;
+	float y;
+	char z;
+	double n;
+	
+	cout<<"size of int : "<<sizeof(x)<<endl;
+	cout<<"size of float : "<<sizeof(y)<<endl;
+	cout<<"size of char : "<<sizeof(z)<<endl;
+	cout<<"size of double : "<<sizeof(n)<<endl;
+	
+	return 0;
+}
